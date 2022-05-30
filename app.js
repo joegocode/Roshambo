849bfb024053306ce6555cca0174638a72dc1e7f
Joel Rodriguez <joegocode@gmail.com>
2022-05-30 13:44:25 -0400

    update

--- app.js
+++ app.js
@@ -1,48 +1,87 @@
+
 let choices = ["rock","paper","scissor"];
 let playerScore = 0;
-let computerScore =0;
+let computerScore = 0;
 
-/* obtain computer/player selection of rock,paper,scissor*/
 function computerPlay(){
     return computerSelection = choices[Math.floor(Math.random() * choices.length)];
 }
 
-/*function for a single round of play alerting results*/
 function singleRound(){
-     if (playerSelection === computerSelection){
-        alert("Its a tie! play again")
-        } else if(playerSelection == 'rock' && computerSelection == 'scissor'){
-            alert('Player wins, Rock beats Scissor!'), ++playerScore;
-        } else if (playerSelection == 'rock' && computerSelection == 'paper'){
-            alert("You lose, Paper beats Rock!"), ++computerScore;
-        }else if (playerSelection == 'scissor' && computerSelection == 'paper'){
-            alert("You win, Scissor beats Paper!"), ++playerScore;
-        }else if (playerSelection == 'scissor' && computerSelection == 'rock'){
-            alert("You lose, Rock beats Scissors!"), ++computerScore;
-        }else if (playerSelection == 'paper' && computerSelection == 'scissor'){
-            alert("You lose, Scissors beats Paper!"), ++computerScore;
-        }else if (playerSelection == 'paper' && computerSelection == 'rock'){
-            alert("You win, Paper beats Rock!"), ++playerScore;
-        }
-    
-}
-/*function that plays five rounds and returns results*/
-function gamePlay(){ 
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt('Choose Rock,paper,Scissor').toLowerCase();
-        computerPlay();
-        console.log(playerSelection)
-        console.log(computerSelection)
-        singleRound();
-    }
-}
- gamePlay();
- console.log(playerScore);
- console.log(computerScore);
+    if (playerSelection === computerSelection){
+    //    alert("Its a tie! play again")
+       } else if(playerSelection == 'rock' && computerSelection == 'scissor'){
+           /*alert('Player wins, Rock beats Scissor!'),*/ ++playerScore;
+       } else if (playerSelection == 'rock' && computerSelection == 'paper'){
+           /*alert("You lose, Paper beats Rock!"),*/ ++computerScore;
+       }else if (playerSelection == 'scissor' && computerSelection == 'paper'){
+           /*alert("You win, Scissor beats Paper!"),*/ ++playerScore;
+       }else if (playerSelection == 'scissor' && computerSelection == 'rock'){
+           /*alert("You lose, Rock beats Scissors!"),*/ ++computerScore;
+       }else if (playerSelection == 'paper' && computerSelection == 'scissor'){
+           /*alert("You lose, Scissors beats Paper!"),*/ ++computerScore;
+       }else if (playerSelection == 'paper' && computerSelection == 'rock'){
+           /*alert("You win, Paper beats Rock!"),*/ ++playerScore;
+       }
 
-/*compare scores and alert winner*/
-if (playerScore > computerScore){
-    alert(`you win!!!,${playerScore} to ${computerScore}`)
-} else if (playerScore < computerScore){
-    alert(`you lose!!! ${playerScore} to ${computerScore}`)
-} else {alert('its a tie, play again')}
+}
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    computerPlay();
+    singleRound()
+
+    const playerPoints = document.querySelector('.userScore')
+    playerPoints.textContent = `Player points: ${playerScore}`;
+
+    const compPoints = document.querySelector('.compScore')
+    compPoints.textContent = `Computer points: ${computerScore}` 
+
+    const final = document.querySelector('.results')
+    if (playerScore >= 5){
+        final.textContent = "you win"
+    } else if (computerScore >=5){
+        final.textContent = "you lose"
+    }
+})
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    computerPlay();
+    singleRound()
+
+    const playerPoints = document.querySelector('.userScore')
+    playerPoints.textContent = `Player points: ${playerScore}`;
+
+    const compPoints = document.querySelector('.compScore')
+    compPoints.textContent = `computer Score: ${computerScore}` 
+
+    const final = document.querySelector('.results')
+    if (playerScore >= 5){
+        final.textContent = "you win";
+    } else if (computerScore >=5){
+        final.textContent = "you lose"
+    } 
+})
+
+const scissor = document.querySelector('#scissor');
+scissor.addEventListener('click', () => {
+    playerSelection = 'scissor';
+    computerPlay();
+    singleRound()
+
+    const playerPoints = document.querySelector('.userScore')
+    playerPoints.textContent = `Player points: ${playerScore}`;
+
+    const compPoints = document.querySelector('.compScore')
+    compPoints.textContent = `computer Score: ${computerScore}` 
+
+    const final = document.querySelector('.results')
+    if (playerScore >= 5){
+        final.textContent = "you win"
+    } else if (computerScore >=5){
+        final.textContent = "you lose"
+    }
+})
